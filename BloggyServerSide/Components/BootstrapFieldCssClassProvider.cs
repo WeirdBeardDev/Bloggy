6a36587cd4047764c4d4caddca23f6eee1775ebc
Aj Comfort <weirdbearddev@gmail.com>
2021-08-26 10:39:47 -0400

    Add custom validation attributes

---
+++ BloggyServerSide/Components/BootstrapFieldCssClassProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BloggyServerSide.Components
+{
+    public class BootstrapFieldCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+            var isModified = editContext.IsModified(fieldIdentifier);
+            return (isModified, isValid) switch
+            {
+                (true, true) => "form-control modified is-valid",
+                (true, false) => "form-control modified is-invalid",
+                (false, true) => "form-control",
+                (false, false) => "form-control"
+            };
+        }
+    }
+}
---
+++ BloggyServerSide/Components/CustomCssClassProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BloggyServerSide.Components
+{
+    public class CustomCssClassProvider<ProviderType>: ComponentBase where ProviderType: FieldCssClassProvider,new()
+    {
+        [CascadingParameter]
+        EditContext? CurrentEditContext { get; set; }
+        public ProviderType Provider { get; set; } = new ProviderType();
+
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException($"{nameof(DataAnnotationsValidator)} requires a cascading parameter of type {nameof(EditContext)}. For example, you can use {nameof(DataAnnotationsValidator)} inside an EditForm.");
+            }
+            CurrentEditContext.SetFieldCssClassProvider(Provider);
+        }
+    }
+}
